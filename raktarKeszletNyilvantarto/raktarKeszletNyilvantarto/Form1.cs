df4f9e4a828a445011f0841b422f6d8d185d2e8f
Fábián Csaba <web.csaba.fabian@gmail.com>
2013-03-06 06:14:34 -0800

    ggg

--- raktarKeszletNyilvantarto/raktarKeszletNyilvantarto/Form1.cs
+++ raktarKeszletNyilvantarto/raktarKeszletNyilvantarto/Form1.cs
@@ -15,7 +15,7 @@ namespace raktarKeszletNyilvantarto
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("hello");
+            MessageBox.Show("Hi");
         }
     }
 }
