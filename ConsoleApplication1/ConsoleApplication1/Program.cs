5529c797c5c017733cad713057db9a85331e37a1
Fábián Csaba <web.csaba.fabian@gmail.com>
2013-03-06 06:02:37 -0800

    teszt

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember Sanyi = new Ember();
+            Ember Feri = new Ember();
+            Ember Kati = new Ember();
+            Ember Pista = new Ember();
+
+            Sanyi.magassag = 180;
+            Sanyi.hajszin = "Kék";
+           string tokmas = Sanyi.adatKiiras();
+           Console.WriteLine(tokmas);
+
+            Kati.magassag = 160;
+            Kati.hajszin = "Piros";
+           Console.WriteLine( Kati.adatKiiras() );
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Ember {
+       public int magassag;
+       public string hajszin;
+
+        public string adatKiiras(){
+
+           string szoveg = "Magasság: " + magassag.ToString() + "\n" + "Hajszin: " + hajszin;
+           return szoveg;
+
+
+        }
+
+       
+
+    }
+}
